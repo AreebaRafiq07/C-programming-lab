8aff6d78829cdccad965cec48a22de8a943b7ce8
AreebaRafiq07 <146534570+AreebaRafiq07@users.noreply.github.com>
2023-11-01 13:24:02 +0530

    Update 3.2 program to check whether the alphabet is vowel or consonant .c

--- 3.2 program to check whether the alphabet is vowel or consonant .c
+++ 3.2 program to check whether the alphabet is vowel or consonant .c
@@ -4,6 +4,6 @@ int main(){
     printf("enter the character = ");
     scanf("%c%c",&value,&value);
     if(value=='a'||value =='e'||value=='i'||value =='o'||value =='u'||value=='A'||value =='E'||value=='I'||value =='O'||value =='U')printf("vowel");
-    else printf("constant");
+    else printf("consonant");
     return 0;
 }
